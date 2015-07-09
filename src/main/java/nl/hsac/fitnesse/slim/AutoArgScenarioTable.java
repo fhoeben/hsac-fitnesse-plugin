77d88a1d3adc8fdb95117a4ed0d5685a9bbcc761
Antoine Lemieux <antoine.lemieux@nuance.com>
2015-07-09 00:25:58 -0400

    fix table template arguments

--- src/main/java/nl/hsac/fitnesse/slim/AutoArgScenarioTable.java
+++ src/main/java/nl/hsac/fitnesse/slim/AutoArgScenarioTable.java
@@ -1,9 +1,8 @@
 package nl.hsac.fitnesse.slim;
 
-import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,9 +21,9 @@ public class AutoArgScenarioTable extends ScenarioTable {
     private final static Pattern ARG_PATTERN = Pattern.compile("@\\{(.+?)\\}");
     private static final Pattern OUT_PATTERN = Pattern.compile("\\$(.+?)=");
 
-    private Set<String> inputs;
-    private Set<String> parameters;
-    private Set<String> outputs;
+    private List<String> inputs;
+    private List<String> tableParameters;
+    private List<String> outputs;
 
     public AutoArgScenarioTable(Table table, String tableId, SlimTestContext testContext) {
         super(table, tableId, testContext);
@@ -38,30 +37,30 @@ public class AutoArgScenarioTable extends ScenarioTable {
     }
 
     @Override
-    protected boolean determineParameterized() {
-      if (this.getTable().getColumnCountInRow(0) > 2){
-        return true;
+  protected void getScenarioArguments() {
+    super.getScenarioArguments();
+    
+    // Store the table arguments for later during Call() method    
+    if (determineParameterized()){
+      String argumentString = table.getCellContents(2, 0);
+      String[] arguments = argumentString.split(",");
+      tableParameters = new LinkedList<String>();
+      for (String args : arguments) {
+        tableParameters.add(args);
       }
-      return false;       
     }
 
-    @Override
-    protected void getScenarioArguments() {         
-        super.getScenarioArguments();
-        
-        parameters = new LinkedHashSet<String>();
-        parameters.addAll(getInputs());
-        
-        for (String input : inputs) {
-            addInput(input);
-        }
-        for (String output : outputs) {
-            addOutput(output);
-        }
+    for (String input : inputs) {
+      addInput(input);
     }
+    
+    for (String output : outputs) {
+      addOutput(output);
+    }
+  }
 
-    private Set<String> findArguments(Pattern pattern) {
-        Set<String> found = new LinkedHashSet<String>();
+    private List<String> findArguments(Pattern pattern) {
+        List<String> found = new LinkedList<String>();
         int rowCount = table.getRowCount();
         for (int row = 0; row < rowCount; row++) {
             int columnCount = table.getColumnCountInRow(row);
@@ -81,13 +80,19 @@ public class AutoArgScenarioTable extends ScenarioTable {
   public List<SlimAssertion> call(final Map<String, String> scenarioArguments, SlimTable parentTable, int row)
       throws SyntaxError {
     
-    // Add the table template parameters to the list of possible arguments along with the profided value
-    int i=0;
-    for (String parameter : parameters) {
-      String value = parentTable.getTable().getCellContents(i+1, 0);
-      scenarioArguments.put(parameter, value);
-      i++;
+    // Insert the matching table parameter values as added scenarioArguments for the running table
+    Table execTable = parentTable.getTable();   
+    if (tableParameters != null){
+      if (execTable.getColumnCountInRow(0) == (tableParameters.size() * 2)){ // should be twice as much cell then parameters -- otherwise we are missing a parameter or a placeholder
+        int i=1;
+        for (String parameter : tableParameters) {      
+          String value = execTable.getCellContents(i, 0);
+          scenarioArguments.put(parameter, value);
+          i = i + 2;
+        }      
+      }
     }
+    
     return super.call(scenarioArguments, parentTable, row);    
   }
 
