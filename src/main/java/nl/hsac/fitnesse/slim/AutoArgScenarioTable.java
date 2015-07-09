c63966a305d99d0f0a1aca991751f345b6b5a228
Antoine Lemieux <antoine.lemieux@nuance.com>
2015-07-08 22:34:41 -0400

    Added capability to provide parameters to the "Table Template"  to be use as default value inside the template for the given placeholder

--- src/main/java/nl/hsac/fitnesse/slim/AutoArgScenarioTable.java
+++ src/main/java/nl/hsac/fitnesse/slim/AutoArgScenarioTable.java
@@ -1,16 +1,18 @@
 package nl.hsac.fitnesse.slim;
 
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import fitnesse.testsystems.slim.SlimTestContext;
 import fitnesse.testsystems.slim.Table;
 import fitnesse.testsystems.slim.tables.ScenarioTable;
 import fitnesse.testsystems.slim.tables.SlimAssertion;
+import fitnesse.testsystems.slim.tables.SlimTable;
 import fitnesse.testsystems.slim.tables.SyntaxError;
-
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * ScenarioTable that looks for in- and output parameters in all its rows, without the
@@ -21,6 +23,7 @@ public class AutoArgScenarioTable extends ScenarioTable {
     private static final Pattern OUT_PATTERN = Pattern.compile("\\$(.+?)=");
 
     private Set<String> inputs;
+    private Set<String> parameters;
     private Set<String> outputs;
 
     public AutoArgScenarioTable(Table table, String tableId, SlimTestContext testContext) {
@@ -36,11 +39,19 @@ public class AutoArgScenarioTable extends ScenarioTable {
 
     @Override
     protected boolean determineParameterized() {
-        return !inputs.isEmpty();
+      if (this.getTable().getColumnCountInRow(0) > 2){
+        return true;
+      }
+      return false;       
     }
 
     @Override
-    protected void getScenarioArguments() {
+    protected void getScenarioArguments() {         
+        super.getScenarioArguments();
+        
+        parameters = new LinkedHashSet<String>();
+        parameters.addAll(getInputs());
+        
         for (String input : inputs) {
             addInput(input);
         }
@@ -65,4 +76,19 @@ public class AutoArgScenarioTable extends ScenarioTable {
         }
         return found;
     }
+    
+  @Override
+  public List<SlimAssertion> call(final Map<String, String> scenarioArguments, SlimTable parentTable, int row)
+      throws SyntaxError {
+    
+    // Add the table template parameters to the list of possible arguments along with the profided value
+    int i=0;
+    for (String parameter : parameters) {
+      String value = parentTable.getTable().getCellContents(i+1, 0);
+      scenarioArguments.put(parameter, value);
+      i++;
+    }
+    return super.call(scenarioArguments, parentTable, row);    
+  }
+
 }
