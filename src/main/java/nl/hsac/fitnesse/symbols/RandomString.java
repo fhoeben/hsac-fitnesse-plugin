09b068d53a1cec03d8985a7c0cfb620345b9e33d
Koen Prins <keetron@gmail.com>
2017-03-26 20:33:30 +0200

    Refactored for readability. Almost completely redid it. Learned a lot actually.

--- src/main/java/nl/hsac/fitnesse/symbols/RandomString.java
+++ src/main/java/nl/hsac/fitnesse/symbols/RandomString.java
@@ -51,46 +51,45 @@ public class RandomString extends SymbolBase implements Rule, Translation {
     public int getRandomStringLength(String param, String prefix) {
         int randomStringLength;
         int minimalLength;
-        int maximalLength;
+        int maximalLength = 0;
         int prefixLength;
+        int randomValue = 0;
 
         //Handle the prefix input parameter
-        if (prefix == "") {
-            prefixLength = 0;
-        } else {
-            prefixLength = prefix.length();
-        }
+        prefixLength = prefix.length();
 
         // Handle the length input parameter
-        if (param == null) { //if there is no parameter, use the default
+        if (param == null) {
             randomStringLength = RANDOM_UTIL.random(100);
-        } else if (!param.contains(",")) { //no comma in the parameter, just parse directly to requestedLength
-            int requestedLength = parseInt(param);
-            if (prefixLength > requestedLength) {
-                throw new IllegalArgumentException("The prefix is longer than the requested string length");
-            }
-            int randomBase = parseInt(param) - prefixLength;
-            randomStringLength = RANDOM_UTIL.random(randomBase);
-        } else { // if there is a comma, there must be range
-            String[] values = param.split(","); //any values after the first two are ignored
-            minimalLength = parseInt(values[0]);
-            maximalLength = parseInt(values[1]);
-
-            if (minimalLength < 0) {
-                throw new IllegalArgumentException("You cannot use a negative value here, nobody wants a negative string");
-            }
-            if (maximalLength < minimalLength) {
-                throw new IllegalArgumentException("Ensure the Max value is higher then the Min value");
-            }
-            if (prefixLength > minimalLength) {
-                throw new IllegalArgumentException("The prefix is longer than the requested minimal string length");
-            }
-
-            int randomBase = maximalLength - minimalLength;
-            int randomValue = RANDOM_UTIL.random(randomBase);
-            randomStringLength = randomValue + minimalLength - prefixLength;
-
+            return randomStringLength;
         }
-        return randomStringLength;
+
+        String[] values = param.split(","); //any values after the first two are ignored
+        minimalLength = parseInt(values[0]);
+
+        if (values.length == 1) {
+            randomValue = RANDOM_UTIL.random(minimalLength) - prefixLength;
+        } else {
+            maximalLength = parseInt(values[1]); // moved this here to prevent trying to fill an int with null
+        }
+
+
+        if (minimalLength < 0) {
+            throw new IllegalArgumentException("You cannot use a negative value here, nobody wants a negative string");
+        }
+        if (maximalLength < minimalLength && maximalLength != 0) {//maxlength is not set to default
+            throw new IllegalArgumentException("Ensure the Max value is higher then the Min value");
+        }
+        if (prefixLength > minimalLength) {
+            throw new IllegalArgumentException("The prefix is longer than the requested minimal string length");
+        }
+
+        if (maximalLength > minimalLength) { //so basically with two normal range indicators
+            int randomBase = maximalLength - minimalLength;
+            randomValue = RANDOM_UTIL.random(randomBase) + minimalLength - prefixLength;
+        }
+
+        return randomValue;
     }
 }
+
