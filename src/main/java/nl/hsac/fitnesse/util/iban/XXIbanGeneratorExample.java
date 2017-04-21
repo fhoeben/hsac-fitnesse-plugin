b53c3a6c8b24a1e052bd94f1d119774eb3703d9b
Fried Hoeben <github@hsac.nl>
2017-04-21 22:18:22 +0200

    Cleanup sample

--- src/main/java/nl/hsac/fitnesse/util/iban/XXIbanGeneratorExample.java
+++ src/main/java/nl/hsac/fitnesse/util/iban/XXIbanGeneratorExample.java
@@ -1,31 +1,27 @@
 package nl.hsac.fitnesse.util.iban;
 
 /**
- * Created by Koen on 20-4-2017.
+ * Sample of what IbanGenerator for country XX looks like.
  */
 public class XXIbanGeneratorExample extends IbanGenerator {
 
     /**
-     * Generates random number to create IBAN.
-     *
-     * @return random Austrian IBAN.
+     * @return random XX IBAN.
      */
 
     public String generateIban(String bankCode) {
-
         String countryCode = "XX";
         int accountLength = 11;
         String accountCodeType = "N";
         int bankCodeLength = 5;
         String bankCodeType = "N";
 
-        bankCode = getBankCode(bankCode, bankCodeList, bankCodeLength, bankCodeType);
+        bankCode = getBankCode(bankCode, BANK_CODE_LIST, bankCodeLength, bankCodeType);
         String account = getAccount(accountLength, accountCodeType);
         String controlNr = getControlNumber(bankCode, account, countryCode);
 
         return countryCode + controlNr + bankCode + account;
-
     }
 
-    public String[] bankCodeList = {};
+    public final static String[] BANK_CODE_LIST = {};
 }
