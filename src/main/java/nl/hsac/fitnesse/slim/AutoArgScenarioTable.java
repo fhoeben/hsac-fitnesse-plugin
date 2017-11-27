6d084350f9d499a495c548f42787dec3ed8b66cf
Fried Hoeben <github@hsac.nl>
2017-11-27 23:33:17 +0100

    Allow nested table template usage (refelective field access should be removed once we have a fitnesse release which includes https://github.com/unclebob/fitnesse/pull/1103)

--- src/main/java/nl/hsac/fitnesse/slim/AutoArgScenarioTable.java
+++ src/main/java/nl/hsac/fitnesse/slim/AutoArgScenarioTable.java
@@ -1,13 +1,17 @@
 package nl.hsac.fitnesse.slim;
 
+import fitnesse.testsystems.TestExecutionException;
 import fitnesse.testsystems.slim.SlimTestContext;
 import fitnesse.testsystems.slim.Table;
 import fitnesse.testsystems.slim.tables.ScenarioTable;
 import fitnesse.testsystems.slim.tables.SlimAssertion;
+import fitnesse.testsystems.slim.tables.SlimTable;
 import fitnesse.testsystems.slim.tables.SyntaxError;
 
+import java.lang.reflect.Field;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,11 +21,13 @@ import java.util.regex.Pattern;
  * parameters having to be specified in the first row also.
  */
 public class AutoArgScenarioTable extends ScenarioTable {
-    private final static Pattern ARG_PATTERN = Pattern.compile("@\\{(.+?)\\}");
+    private static final Pattern ARG_PATTERN = Pattern.compile("@\\{(.+?)\\}");
     private static final Pattern OUT_PATTERN = Pattern.compile("\\$(.+?)=");
 
     private Set<String> inputs;
     private Set<String> outputs;
+
+    private Map<String, String> currentCallArguments;
 
     public AutoArgScenarioTable(Table table, String tableId, SlimTestContext testContext) {
         super(table, tableId, testContext);
@@ -56,13 +62,90 @@ public class AutoArgScenarioTable extends ScenarioTable {
             int columnCount = table.getColumnCountInRow(row);
             for (int column = 0; column < columnCount; column++) {
                 String cellContent = table.getCellContents(column, row);
-                Matcher m = pattern.matcher(cellContent);
-                while (m.find()) {
-                    String input = m.group(1);
-                    found.add(input);
+                if (!addAllMatches(pattern, found, cellContent)
+                        && maybeNestedScenario(columnCount, cellContent)) {
+                    addNestedScenarioArguments(found, pattern == ARG_PATTERN, cellContent);
                 }
             }
         }
         return found;
+    }
+
+    private void addNestedScenarioArguments(Set<String> found, boolean addInputs, String cellContent) {
+        String scenarioName = cellContent.substring(0, cellContent.length() - 1);
+        ScenarioTable scenario = getTestContext().getScenario(scenarioName);
+        if (scenario != null) {
+            Set<String> scenarioArgs = addInputs ? scenario.getInputs() : scenario.getOutputs();
+            found.addAll(scenarioArgs);
+        }
+    }
+
+    private boolean maybeNestedScenario(int columnCount, String cellContent) {
+        return columnCount == 1 && cellContent.endsWith(";");
+    }
+
+    private boolean addAllMatches(Pattern pattern, Set<String> found, String cellContent) {
+        boolean anyMatches = false;
+        Matcher m = pattern.matcher(cellContent);
+        while (m.find()) {
+            String input = m.group(1);
+            found.add(input);
+            anyMatches = true;
+        }
+        return anyMatches;
+    }
+
+    public Map<String, String> getCurrentCallArguments() {
+        return currentCallArguments;
+    }
+
+    @Override
+    public List<SlimAssertion> call(Map<String, String> scenarioArguments,
+                                    SlimTable parentTable, int row) throws TestExecutionException {
+        try {
+            currentCallArguments = scenarioArguments;
+            if (scenarioArguments.isEmpty()) {
+                SlimTestContext context = parentTable.getTestContext();
+                if (context instanceof ScenarioTestContext) {
+                    ScenarioTestContext sTestContext = (ScenarioTestContext) context;
+                    AutoArgScenarioTable caller = getCallingTable(sTestContext);
+                    Map<String, String> callerArgs = caller.getCurrentCallArguments();
+                    for (Map.Entry<String, String> entry : callerArgs.entrySet()) {
+                        String arg = entry.getKey();
+                        if (inputs.contains(arg)) {
+                            scenarioArguments.put(arg, entry.getValue());
+                        }
+                    }
+                }
+            }
+            return super.call(scenarioArguments, parentTable, row);
+        } finally {
+            currentCallArguments = null;
+        }
+    }
+
+    // TODO : Remove usage of reflective field access once https://github.com/unclebob/fitnesse/pull/1103 is released
+    private static final Field stField;
+
+    static {
+        try {
+            stField = ScenarioTestContext.class.getDeclaredField("this$0");
+            stField.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private AutoArgScenarioTable getCallingTable(ScenarioTestContext context) {
+        try {
+            ScenarioTable t = (ScenarioTable) stField.get(context);
+            if (t instanceof AutoArgScenarioTable) {
+                return (AutoArgScenarioTable) t;
+            } else {
+                return null;
+            }
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
