845e7f1e5c9b8c96264c6fc629d2800413ec7f52
Fried Hoeben <github@hsac.nl>
2017-11-29 07:55:46 +0100

    Add unit test for AutoArgScenarioTable

---
+++ src/test/java/nl/hsac/fitnesse/slim/tables/WikiBasedTest.java
@@ -0,0 +1,11 @@
+package nl.hsac.fitnesse.slim.tables;
+
+import fitnesse.junit.FitNesseRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(FitNesseRunner.class)
+@FitNesseRunner.Suite("HsacPlugin")
+@FitNesseRunner.FitnesseDir("./src/test/wiki")
+@FitNesseRunner.OutputDir("./target/fitnesse-results")
+public class WikiBasedTest {
+}
