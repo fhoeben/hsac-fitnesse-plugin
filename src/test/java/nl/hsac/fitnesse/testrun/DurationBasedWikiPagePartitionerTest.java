62d21cf9e5d9920d829c14d9f34cf96639e42f96
Fried Hoeben <github@hsac.nl>
2020-03-08 21:50:05 +0100

    ensure empty durations is handled properly

--- src/test/java/nl/hsac/fitnesse/testrun/DurationBasedWikiPagePartitionerTest.java
+++ src/test/java/nl/hsac/fitnesse/testrun/DurationBasedWikiPagePartitionerTest.java
@@ -5,6 +5,8 @@ import fitnesse.wiki.WikiPageDummy;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -40,6 +42,34 @@ public class DurationBasedWikiPagePartitionerTest {
         assertEquals(112349L, result.get(1).stream().mapToLong(p -> timePerTest.get(geName(p))).sum());
         assertEquals(112348L, result.get(2).stream().mapToLong(p -> timePerTest.get(geName(p))).sum());
         assertEquals(112348L, result.get(3).stream().mapToLong(p -> timePerTest.get(geName(p))).sum());
+    }
+
+    @Test
+    public void testDurationSplitNoDurations() {
+        List<DurationRecord<String>> records = Collections.emptyList();
+        WikiPageDummy root = new WikiPageDummy("root", "root page", null);
+        List<WikiPage> names = Arrays.asList("Page1", "Page2", "Page3").stream()
+                .map(n -> new WikiPageDummy(n, n, root))
+                .collect(Collectors.toList());
+
+        DurationBasedWikiPagePartitioner partitioner = new DurationBasedWikiPagePartitioner(records);
+
+        List<List<WikiPage>> result = partitioner.split(names, 2);
+
+        assertEquals(2, result.size());
+
+        List<WikiPage> all = new ArrayList<>(names.size());
+        result.forEach(all::addAll);
+        assertEquals(names.size(), all.size());
+
+        List<WikiPage> part0 = result.get(0);
+        assertEquals(2, part0.size());
+        assertEquals("Page1", geName(part0.get(0)));
+        assertEquals("Page2", geName(part0.get(1)));
+
+        List<WikiPage> part1 = result.get(1);
+        assertEquals(1, part1.size());
+        assertEquals("Page3", geName(part1.get(0)));
     }
 
     private String geName(WikiPage p) {
