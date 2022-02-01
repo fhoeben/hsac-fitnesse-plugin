a683bb0ecb1058972da1a967de5ce188d0f8ab5a
splpws <pawel.szymczak@sas.com>
2022-02-01 09:19:20 +0100

    Added functionality to pass input arguments to child scenarios.

--- src/main/java/nl/hsac/fitnesse/slim/AllArgScenarioTable.java
+++ src/main/java/nl/hsac/fitnesse/slim/AllArgScenarioTable.java
@@ -4,11 +4,14 @@ import fitnesse.testsystems.TestExecutionException;
 import fitnesse.testsystems.slim.HtmlTable;
 import fitnesse.testsystems.slim.SlimTestContext;
 import fitnesse.testsystems.slim.Table;
+import fitnesse.testsystems.slim.tables.ScenarioTable;
 import fitnesse.testsystems.slim.tables.SlimAssertion;
 import fitnesse.testsystems.slim.tables.SlimTable;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * Scenario table that allows usage @{*} symbol to expand all input variables as a constructor parameter for
@@ -16,6 +19,7 @@ import java.util.Map;
  */
 public class AllArgScenarioTable extends AutoArgScenarioTable {
     private static final String ALL_ARG_PATTERN = "@{*}";
+    private Map<String,String> inputArgs;
 
     public AllArgScenarioTable(Table table, String tableId, SlimTestContext testContext) {
         super(table, tableId, testContext);
@@ -24,6 +28,9 @@ public class AllArgScenarioTable extends AutoArgScenarioTable {
     @Override
     public List<SlimAssertion> call(Map<String, String> scenarioArguments,
                                     SlimTable parentTable, int row) throws TestExecutionException {
+        if(scenarioArguments.size() == 0){
+            scenarioArguments.putAll(getInputArgs(parentTable,this.getTestContext()));
+        }
         /* pass all input arguments to super inputs to bypass "Variables not used in scenario" error */
         for(Map.Entry<String,String> entry : scenarioArguments.entrySet()){
             if(!super.getInputs().contains(entry.getKey())){
@@ -45,7 +52,7 @@ public class AllArgScenarioTable extends AutoArgScenarioTable {
                 }
             }
         }
-
+        this.inputArgs = scenarioArguments;
         /* call super method */
         return super.call(scenarioArguments,parentTable,row);
     }
@@ -54,6 +61,18 @@ public class AllArgScenarioTable extends AutoArgScenarioTable {
         if(scenarioArguments.entrySet().size() == 0) throw new TestExecutionException("Scenario has no input arguments.");
         String out = scenarioArguments.entrySet().stream().map(e -> e.getKey()+","+"@{"+e.getKey()+"}").reduce((e,v) -> e = e.concat(","+v)).get();
         return "["+out+"]";
+    }
+
+    private Map<String,String> getInputArgs(SlimTable parentTable, SlimTestContext context){
+        HtmlTable parentHtml = (HtmlTable)parentTable.getTable();
+        try{
+            String parentid = parentHtml.getCellContents(1,0);
+            ScenarioTable scenarioTable = context.getScenario(parentid);
+            if(scenarioTable instanceof AllArgScenarioTable) return ((AllArgScenarioTable) scenarioTable).inputArgs;
+            else return null;
+        } catch(IndexOutOfBoundsException e){
+            return null;
+        }
     }
 
 }
